10059be045f63431f9de43d32243da625a300ce8
Onyeriri Victor <viktorchinonso@gmail.com>
2022-11-07 20:07:53 +0000

    Start reusing component in home components

--- src/Home.js
+++ src/Home.js
@@ -4,12 +4,16 @@ import BlogList from "./BlogList";
 const Home = () => {
   const [blogs, setBlogs] = useState([
     { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
-    { title: "Welcome party!", body: "lorem ipsum...", author: "mario", id: 2 },
-    { title: "Web dev tips", body: "lorem ipsum...", author: "mario", id: 3 },
+    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
+    { title: "Web dev tips", body: "lorem ipsum...", author: "victor", id: 3 },
   ]);
   return (
     <div className="home">
       <BlogList blogs={blogs} title={"All blogs!"} />
+      <BlogList
+        blogs={blogs.filter((blog) => blog.author === "mario")}
+        title={"Mario blogs!"}
+      />
     </div>
   );
 };
