d00caaa0d2ae7946b8c43f92db3bf56c8cfb46e3
Onyeriri Victor <viktorchinonso@gmail.com>
2022-11-07 19:43:16 +0000

    Introduced props into Blog list component

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState([
@@ -8,16 +9,7 @@ const Home = () => {
   ]);
   return (
     <div className="home">
-      {blogs.map((blog) => {
-        return (
-          <div className="blog-preview" key={blog.id}>
-            <h2>{blog.title}</h2>
-            <p>{blog.body}</p>
-            <p>{blog.author}</p>
-            <p>{blog.id}</p>
-          </div>
-        );
-      })}
+      <BlogList blogs={blogs} title={"All blogs!"} />
     </div>
   );
 };
