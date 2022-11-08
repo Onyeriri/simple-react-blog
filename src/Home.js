e513ed1171c97e216f44e79426167eedcdd88f89
Onyeriri Victor <viktorchinonso@gmail.com>
2022-11-08 09:40:02 +0000

    Introduced error handling on fetch api

--- src/Home.js
+++ src/Home.js
@@ -3,20 +3,30 @@ import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch("http://localhost:8000/blogs")
-      .then((response) => response.json())
+      .then((response) => {
+        if (!response.ok) {
+          throw Error(
+            "could not fetch the data from http://localhost:8000/blogs"
+          );
+        }
+        return response.json();
+      })
       .then((data) => setBlogs(data))
-      .catch((err) => err.massage);
+      .catch((err) => setError(err.message));
   }, []);
 
   return (
     <div className="home">
       {blogs ? (
         <BlogList blogs={blogs} title={"All blogs!"} />
+      ) : <h1>Loading...</h1> ? (
+        error
       ) : (
-        <h1>Loading...</h1>
+        error
       )}
     </div>
   );
