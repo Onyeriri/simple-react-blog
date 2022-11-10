7723eec6e2fb19840a06223922a3576db7466f56
Onyeriri Victor <viktorchinonso@gmail.com>
2022-11-10 07:32:22 +0000

    Added pragmmatic redirect inside create component

--- src/Create.js
+++ src/Create.js
@@ -1,10 +1,11 @@
 import { useState } from "react";
-
+import { useHistory } from "react-router-dom";
 const Create = () => {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const [author, setAuthor] = useState("mario");
   const [isLoading, setIsLoading] = useState(false);
+  const history = useHistory();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -24,6 +25,7 @@ const Create = () => {
     }).then(() => {
       console.log("blog created");
       setIsLoading(false);
+      history.push("/");
     });
 
     setTitle("");
