a2e8d926f59764075adf9c6309df7b58d3ae521e
Onyeriri Victor <viktorchinonso@gmail.com>
2022-11-10 07:59:02 +0000

    Added deleting functionality to bloglist component

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,11 +1,22 @@
 import { useParams } from "react-router-dom";
 import useFetch from "./useFetch";
+import { useHistory } from "react-router-dom";
 
 const BlogDetails = () => {
   const { id } = useParams();
+  const history = useHistory();
 
   const url = `http://localhost:8000/blogs/${id}`;
   const { data } = useFetch(url);
+
+  const handleDelete = () => {
+    fetch(`http://localhost:8000/blogs/${id}`, {
+      method: "DELETE",
+    }).then(() => {
+      console.log("Post deleted...");
+      history.push("/");
+    });
+  };
 
   return (
     <div className="blog-details">
@@ -18,6 +29,7 @@ const BlogDetails = () => {
           <p>
             <b>written by {data.author}</b>
           </p>
+          <button onClick={handleDelete}>Delete post</button>
         </article>
       )}
     </div>
